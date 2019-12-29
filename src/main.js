6e7cf0c6123234b93418c4691f9524053c8c1151
Ruy Garcia <rrgarciach@gmail.com>
2019-12-28 22:41:52 -0700

    First commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import BootstrapVue from 'bootstrap-vue';
+import 'vue-awesome/icons';
+import Icon from 'vue-awesome/components/Icon';
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
